577ad7e45d76cd31fbe01ce4d60f2367dd7c2f2f
Dan Bonomo <d.z.bonomo@gmail.com>
2012-12-17 13:20:16 -0700

    Adds Dynamic Menu area

--- footer.php
+++ footer.php
@@ -1,24 +1,8 @@
 				<div id="footer">
-<?php
-$include_footer_list = get_option('include_footer_list');
-if ($include_footer_list != "") {
-?>
 					<p class="floatleft">
-<?php
-	$include_footer_list = explode(",",$include_footer_list);
-	$output;
-	foreach ($include_footer_list as $page) {
-		$output .= "						<a href='" . get_permalink($page) . "' title='" . get_the_title($page) . "'>" . get_the_title($page) . "</a> | \n";
-	}
-	$output = substr($output, 0, -4);
-	$output .= "\n";
-	echo $output;
-?>
+<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
 					</p>
 					<div class="clearboth"></div>
-<?php 
-}
-?>
 				</div>
 			</div>
 		</div>
